f6584d5fbc195a4861d0941a620a8018397c7735
itzRoma <romabondarenko03@gmail.com>
2023-02-02 21:49:36 +0200

    initial commit
    - initialized project using spring initializr
    - configured local database using docker
    - created rest controller with simple "hello" method

---
+++ src/main/java/org/sevenorganization/int20h2023ttbe/Int20H2023TtBeApplication.java
@@ -0,0 +1,24 @@
+package org.sevenorganization.int20h2023ttbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Int20H2023TtBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Int20H2023TtBeApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(
+            @RequestParam(name = "name", required = false, defaultValue = "world") String name
+    ) {
+        return ResponseEntity.ok("hello " + name);
+    }
+}
---
+++ src/test/java/org/sevenorganization/int20h2023ttbe/Int20H2023TtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sevenorganization.int20h2023ttbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int20H2023TtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
